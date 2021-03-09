3081c6c3df37e4bba1226c62c7f7a10f8698df6a
Buchyk <77931586+Buchyk@users.noreply.github.com>
2021-03-09 12:34:15 +0200

    Update ApplicationConfig.js

--- src/config/ApplicationConfig.js
+++ src/config/ApplicationConfig.js
@@ -1,4 +1,4 @@
-export const ROOT_SERVER = "http://${DESTIONATIO_IP}:8080";
+export const ROOT_SERVER = "http://3.65.83.213:8080";
 export const ROOT_URI = "/dev";
 
 export const BASE_URL = ROOT_SERVER + ROOT_URI;
